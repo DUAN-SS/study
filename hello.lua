a92fff5fce980bc2008960c1768bcc52ea7a345a
DUAN-SS <2980946241@qq.com>
2020-05-02 16:42:18 +0800

    提交一个lua脚本文件

---
+++ hello.lua
@@ -0,0 +1,20 @@
+--[[print(type("Hello world"))      --> string
+print(type(10.4*3))             --> number
+print(type(print))              --> function
+print(type(type))               --> function
+print(type(true))               --> boolean
+print(type(nil))                --> nil
+print(type(type(X)))            --> string
+
+
+print(type(a))
+--]]
+html = [[
+<html>
+<head></head>
+<body>
+    <a href="http://www.runoob.com/">²ËÄñ½Ì³Ì</a>
+</body>
+</html>
+]]
+print(html)
